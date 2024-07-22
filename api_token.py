6cb73c4046d8180091796fd5cf0f22a03a758391
syedhamidali <hamidsyed37@gmail.com>
2024-07-22 00:31:48 -0400

    update token retrieve

--- api_token.py
+++ api_token.py
@@ -1,7 +1,7 @@
 import os
 
 def api_key():
-    return os.getenv('TELEGRAM_BOT_TOKEN')
-
-# apikey = api_key()
-# print(apikey)
+    token = os.getenv('TELEGRAM_BOT_TOKEN')
+    if not token:
+        raise ValueError("TELEGRAM_BOT_TOKEN is not set")
+    return token
