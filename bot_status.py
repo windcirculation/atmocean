e371a8a5505dbe658da95b37c3d53955f02bc2ab
syedhamidali <hamidsyed37@gmail.com>
2024-07-22 00:24:50 -0400

    update bot status

--- bot_status.py
+++ bot_status.py
@@ -1,6 +1,27 @@
 from es_jobs_net import esjobs
 from met_jobs import metjobs
 from egu_jobs import egujobs
+from api_token import api_key
+import random
+
+def mask_random_positions(token, num_positions):
+    token_list = list(token)
+    token_length = len(token_list)
+    positions = random.sample(range(token_length), num_positions)
+    for pos in positions:
+        token_list[pos] = 'X'
+    return ''.join(token_list)
+
+# Retrieve the bot token
+bot_token = api_key()
+
+# Define the number of positions to mask
+num_positions_to_mask = 15  # Adjust this number as needed
+
+# Mask the token at random positions
+masked_bot_token = mask_random_positions(bot_token, num_positions_to_mask)
+
+print(masked_bot_token)
 
 esjobs(post_jobs=False, jobbot_status=True)
 print("Posted E_JOBS")
@@ -10,4 +31,3 @@ print("Posted Met-Jobs")
 
 egujobs(post_jobs=False, jobbot_status=True)
 print("Posted EGU-Jobs")
-
